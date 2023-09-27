f273956fb530c3c925a7851dfb0d09c0abbf53d1
Timothy Egans <94965433+timothyegans1@users.noreply.github.com>
2023-09-26 22:37:22 -0500

    changes added

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,5 @@
-<script src="https://kit.fontawesome.com/22b1aec6e8.js" crossorigin="anonymous"></script>
-
-const textElement = document.getElementById('text');
-const textToType = "Hello World!";
+const textElement = document.getElementById('typingText');
+const textToType = "Timothy Egans";
 let index = 0;
 
 function typeText() {
@@ -9,10 +7,14 @@ function typeText() {
         textElement.textContent += textToType.charAt(index);
         index++;
         setTimeout(typeText, 200); // Adjust the speed of typing by changing this value
+    } else {
+        // Start blinking cursor effect after typing is finished
+        textElement.classList.add('blinking-cursor');
     }
 }
 
 typeText();
+
 
 var scroll = new SmoothScroll('a[href*="#"]', {
     speed: 1000,
@@ -21,3 +23,6 @@ var scroll = new SmoothScroll('a[href*="#"]', {
   }
   )
   ;
+
+
+  
