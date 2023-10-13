c513d40beb61f2ff616952fcc23d9a6821b5a69e
Timothy Egans <94965433+timothyegans1@users.noreply.github.com>
2023-10-13 00:38:40 -0500

    added dark mode switcher to home page

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,25 +4,36 @@ let index = 0;
 
 function typeText() {
     if (index < textToType.length) {
-        textElement.textContent += textToType.charAt(index);
+        textElement.textContent += textToType.charAt(index)
         index++;
-        setTimeout(typeText, 200); // Adjust the speed of typing by changing this value
+        setTimeout(typeText, 200) // Adjust the speed of typing by changing this value
     } else {
         // Start blinking cursor effect after typing is finished
-        textElement.classList.add('blinking-cursor');
+        textElement.classList.add('blinking-cursor')
     }
 }
 
 typeText();
 
 
-var scroll = new SmoothScroll('a[href*="#"]', {
-    speed: 1000,
-    speedAsDuration: true,
+
+
+
+  //switch function
+  const switchTheme = () => {
+    //get root element and data-theme attribute
+    const rootElem = document.documentElement;
+    let dataTheme = rootElem.getAttribute('data-theme'),
+        newTheme
+    newTheme = (dataTheme === 'light') ? 'dark' : 'light'
+
+    //set new data-theme attribute
+    rootElem.setAttribute('data-theme', newTheme)
+    //set new local storage item
+    localStorage.setItem('theme', newTheme)
 
   }
-  )
-  ;
 
 
-  
+  //add event listener
+  document.querySelector('#theme-switcher').addEventListener('click', switchTheme)
